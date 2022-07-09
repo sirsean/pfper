a33d921f14785cf3e58471050ed83fe4f56ee720
sirsean <sirsean@gmail.com>
2022-07-09 07:25:27 -0500

    refactor editor

--- src/App.js
+++ src/App.js
@@ -1,96 +1,16 @@
 import React from 'react';
-import { Provider, useSelector } from 'react-redux';
+import { Provider } from 'react-redux';
 import {
     BrowserRouter as Router,
     Routes,
     Route,
 } from 'react-router-dom';
 import './App.css';
-import { GRID_SIZE, CELL_SIZE, COLORS } from './constants.js';
-import { store, actions, selectors } from './database.js';
+import { store } from './database.js';
 import Home from './views/home.js';
 import Address from './views/address.js';
 import Token from './views/token.js';
-import ColorPicker from './views/color_picker.js';
-import MintBar from './views/mint_bar.js';
-
-const {
-    setColor,
-} = actions;
-
-const {
-    selectColorMatrix,
-} = selectors;
-
-function svgPaint(p, elem) {
-    const rect = elem.getBoundingClientRect();
-    const x = Math.floor((p.x - rect.left) / CELL_SIZE);
-    const y = Math.floor((p.y - rect.top) / CELL_SIZE);
-    store.dispatch(setColor({ x, y }));
-}
-
-function svgOnClick(e) {
-    const point = {
-        x: e.clientX,
-        y: e.clientY,
-    };
-    svgPaint(point, e.target.parentElement);
-}
-
-function svgOnMouseMove(e) {
-    if (e.buttons > 0) {
-        const point = {
-            x: e.clientX,
-            y: e.clientY,
-        };
-        svgPaint(point, e.target.parentElement);
-    }
-}
-
-function svgOnTouchMove(e) {
-    const touch = e.touches[0];
-    const point = {
-        x: touch.clientX,
-        y: touch.clientY,
-    }
-    svgPaint(point, e.target.parentElement);
-}
-
-function PfpSvg() {
-    const cm = useSelector(selectColorMatrix);
-    const viewBox = `0 0 ${GRID_SIZE} ${GRID_SIZE}`;
-    const k = (x, y) => (`${x},${y}`);
-    return (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox={viewBox}
-                onClick={svgOnClick}
-                onMouseMove={svgOnMouseMove}
-                onTouchMove={svgOnTouchMove}>
-            {cm.map((row, x) => row.map((colorIndex, y) => <rect key={k(x,y)} width="1" height="1" x={x} y={y} fill={COLORS[colorIndex]} />)).flat()}
-        </svg>
-    );
-}
-
-function Pfp() {
-    return (
-        <div className="pfp" style={{width: GRID_SIZE * CELL_SIZE, height: GRID_SIZE * CELL_SIZE}}>
-            <PfpSvg />
-        </div>
-    );
-}
-
-function Editor() {
-    const block = (e) => {
-        e.preventDefault();
-        return false;
-    }
-    return (
-        <div className="Editor" onContextMenu={block}>
-            <MintBar />
-            <ColorPicker />
-            <Pfp />
-        </div>
-    );
-}
+import Editor from './views/editor.js';
 
 function App() {
     return (
